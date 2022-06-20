8b6049167a261a8771f859ba50100f5af70a51aa
saruay <644608020001@dpu.ac.th>
2022-06-20 15:48:40 +0700

    ้ร้ร

---
+++ oop2/modulepackage/call_cafe_module2.py
@@ -0,0 +1,12 @@
+import cafe
+dessert_menu = cafe.Dessert()
+print(dessert_menu.show_dessrt())
+
+#แสดงรายการเครื่องดื่มของร้าน
+drink_menu = cafe.Drink()
+print(drink_menu.show_drink())
+
+#เพิ่มรายการเครื่องดื่ม
+drink_menu.add_drink('juice')
+drink_menu.add_drink('smoothy')
+print(drink_menu.show_drink())
