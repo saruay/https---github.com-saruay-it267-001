d141762048f172cf564373266bd51fa0bed86169
saruay <644608020001@dpu.ac.th>
2022-06-21 14:30:58 +0700

    omggggg

---
+++ oop2/inheritance/empit.py
@@ -0,0 +1,23 @@
+from employee import Employee
+class EmpIT(Employee):
+    def __init__(self, id, name, salary) -> None:
+        super().__init__(id, name, salary)
+        self.skill = None
+        self.experience = None
+        self.department = 'IT'
+
+    def add_skill(self,skill:str):
+        self.skill = skill
+
+    def add_experience(self,experience:int):
+        self.experience = experience
+
+    def emp_detail(self):
+        print('------ Employee IT Detail ------')
+        super().emp_detail()
+         #เรียกใช้ method emp_detail ของคลาส Employee เพื่อแสดง id,name
+        print(f'skill: {self.skill}')
+        print(f'experience: {self.emp_detail} year(s)')
+
+    def it_salary(self):
+        self._emp_salary()
