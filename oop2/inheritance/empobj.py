aa05d29c5cbfb9067c09977b64f485df978665f6
saruay <644608020001@dpu.ac.th>
2022-06-21 14:50:37 +0700

    sdfs

---
+++ oop2/inheritance/empobj.py
@@ -0,0 +1,8 @@
+from empit import EmpIT
+
+#create object employee IT
+mike = EmpIT("001","Mike",6000)
+mike.add_skill("Python,JavaSrcipt")
+mike.add_experience(5)
+mike.emp_detail()
+mike.it_salary()
