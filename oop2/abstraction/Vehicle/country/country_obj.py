f0b61350857553805d9666781178eb60e0b5790c
saruay <644608020001@dpu.ac.th>
2022-07-04 14:22:42 +0700

    oh omg

---
+++ oop2/abstraction/Vehicle/country/country_obj.py
@@ -0,0 +1,12 @@
+from country_subclass import *
+
+#create object europe
+eu = Europe("Norway",5505859)
+eu.location = "60.4720° N, 8.4689° E"
+eu.capital = "Oslo"
+eu.show_detail()
+
+#create object asia
+a = Asia("Malaysia",33181072)
+a.gdp = 336.7
+a.show_detail()
